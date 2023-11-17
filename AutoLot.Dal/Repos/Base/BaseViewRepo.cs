20452ec06495f27ffb4ea38df81a43cb95ca23af
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-11-17 19:18:24 +0200

    Created base repos

---
+++ AutoLot.Dal/Repos/Base/BaseViewRepo.cs
@@ -0,0 +1,56 @@
+﻿
+namespace AutoLot.Dal.Repos.Base;
+public abstract class BaseViewRepo<T> : IBaseViewRepo<T> 
+    where T : class, new()
+{
+    private readonly bool _disposeContext;
+    public ApplicationDbContext Context { get; }
+
+    public DbSet<T> Table { get; }
+
+    protected BaseViewRepo(ApplicationDbContext context)
+    {
+        Context = context;
+        Table = Context.Set<T>();
+        _disposeContext = false;
+    }
+
+    protected BaseViewRepo(DbContextOptions<ApplicationDbContext> options) : this(new ApplicationDbContext(options))
+    {
+        _disposeContext = true;
+    }
+
+    ~BaseViewRepo() 
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private bool _isDisposed;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_isDisposed)
+        {
+            return;
+        }
+        if (disposing)
+        {
+            if (_disposeContext)
+            {
+                Context.Dispose();
+            }
+        }
+        _isDisposed = true;
+    }
+
+    public IEnumerable<T> ExecuteSqlString(string sql) => Table.FromSqlRaw(sql);
+
+    public IEnumerable<T> GetAll() => Table.AsQueryable();
+
+    public IEnumerable<T> GetAllIgnoreQueryFilters() => Table.AsQueryable().IgnoreQueryFilters();
+}
---
+++ AutoLot.Dal/Repos/Base/TemporalTableBaseRepo.cs
@@ -0,0 +1,44 @@
+﻿namespace AutoLot.Dal.Repos.Base;
+public abstract class TemporalTableBaseRepo<T> : BaseRepo<T>, ITemporalTableBaseRepo<T> 
+    where T : BaseEntity, new()
+{
+    protected TemporalTableBaseRepo(ApplicationDbContext context) : base(context) { }
+    protected TemporalTableBaseRepo(DbContextOptions<ApplicationDbContext> options)
+        : this(new ApplicationDbContext(options)) { }
+
+    public IEnumerable<TemporalViewModel<T>> GetAllHistory()
+        => ExecuteQuery(Table.TemporalAll());
+
+    public IEnumerable<TemporalViewModel<T>> GetHistoryAsOf(DateTime dateTime)
+        => ExecuteQuery(Table.TemporalAsOf(ConvertToUtc(dateTime)));
+
+    public IEnumerable<TemporalViewModel<T>> GetHistoryBetween(
+    DateTime startDateTime, DateTime endDateTime)
+        => ExecuteQuery(Table.TemporalBetween(ConvertToUtc(startDateTime), ConvertToUtc(endDateTime)));
+
+    public IEnumerable<TemporalViewModel<T>> GetHistoryContainedIn(
+    DateTime startDateTime, DateTime endDateTime)
+        => ExecuteQuery(Table.TemporalContainedIn(ConvertToUtc(startDateTime), ConvertToUtc(endDateTime)));
+
+    public IEnumerable<TemporalViewModel<T>> GetHistoryFromTo(DateTime startDateTime, DateTime endDateTime)
+        => ExecuteQuery(Table.TemporalFromTo(ConvertToUtc(startDateTime), ConvertToUtc(endDateTime)));
+
+    #region Helper methods
+    internal static DateTime ConvertToUtc(DateTime dateTime)
+        => TimeZoneInfo.ConvertTimeToUtc(dateTime, TimeZoneInfo.Local);
+
+    /// <summary>
+    /// Takes in an IQueryable<T>, adds the OrderBy clause for the ValidFrom field, and projects the results into a collection of TemporalViewModel instances
+    /// </summary>
+    /// <param name="query"></param>
+    /// <returns></returns>
+    internal static IEnumerable<TemporalViewModel<T>> ExecuteQuery(IQueryable<T> query)
+        => query.OrderBy(e => EF.Property<DateTime>(e, "ValidFrom"))
+                    .Select(e => new TemporalViewModel<T>
+                    {
+                        Entity = e,
+                        ValidFrom = EF.Property<DateTime>(e, "ValidFrom"),
+                        ValidTo = EF.Property<DateTime>(e, "ValidTo")
+                    });
+    #endregion
+}
