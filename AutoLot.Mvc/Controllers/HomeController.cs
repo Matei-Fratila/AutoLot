0b9a0e9de3e2c26348c334ba3a359763e1d82cc2
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-11-23 21:43:32 +0200

    Added some Display/Editor templates and broke layout into partials

--- AutoLot.Mvc/Controllers/HomeController.cs
+++ AutoLot.Mvc/Controllers/HomeController.cs
@@ -29,4 +29,10 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpGet]
+    public async Task<IActionResult> RazorSyntaxAsync()
+    {
+        return View();
+    }
 }
