9c2aaf1e2973b0636bdfe1c52c76b40fffd7ba68
Frățilă Valeriu-Matei <33498828+Matei-Fratila@users.noreply.github.com>
2023-11-25 22:44:55 +0200

    Added menu view component for Makes

---
+++ AutoLot.Mvc/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoLot.Mvc.ViewComponents;
+
+public class MenuViewComponent : ViewComponent
+{
+    private readonly IMakeDataService _dataService;
+    public MenuViewComponent(IMakeDataService dataService)
+    {
+        _dataService = dataService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var makes = (await _dataService.GetAllAsync()).ToList();
+        if (!makes.Any())
+        {
+            return new ContentViewComponentResult("Unable to get the makes");
+        }
+        return View("MenuView", makes);
+    }
+}
